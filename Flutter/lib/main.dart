e609267348380c256d359fd272fc557a6ee4be10
Abu <abubakaribm@gmail.com>
2025-11-15 20:47:19 +0000

    AF_FirmwareNoUpdate

--- Flutter/lib/main.dart
+++ Flutter/lib/main.dart
@@ -1473,7 +1473,7 @@ class _EPaperImageSenderState extends State<EPaperImageSender> with SingleTicker
         
         // If no version in filename, you could read it from binary metadata
         // For now, return a default version that you should update manually
-        return "1.36.0"; // Update this when you create new OTA files
+        return "1.37.0"; // Update this when you create new OTA files
         
       } catch (e) {
         print('OTA file not found: $e');
